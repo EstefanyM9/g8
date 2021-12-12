7e704691f47e531f36814383614ef62a80153259
EstefanyM9 <91507176+EstefanyM9@users.noreply.github.com>
2021-12-12 10:28:53 -0500

    first commit

---
+++ src/main/java/com/example/g8/repositoriesCrud/ClotheCrudRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.g8.repositoriesCrud;
+
+import com.example.g8.entities.Clothe;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface ClotheCrudRepository extends MongoRepository<Clothe,Integer>{
+    
+    Optional<Clothe> findByReference(String reference);
+    
+}
---
+++ src/main/java/com/example/g8/repositoriesCrud/personaCrudRepository.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.g8.repositoriesCrud;
+
+import com.example.g8.entities.Persona;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface personaCrudRepository extends MongoRepository<Persona, Integer>{
+    
+}
